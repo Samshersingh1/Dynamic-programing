618b43b9220db0f673b8d426277850ecc593819f
harshit <samshersinghbaghel152@gmail.com>
2025-01-23 09:35:39 +0530

    basic

--- MCM PATTERN/Basic.cpp
+++ MCM PATTERN/Basic.cpp
@@ -15,7 +15,55 @@
   step 1 :: recongnise boundry where we have to do partition (i,j) genearrly it will be (0,n-1) but not all the time may be varry question by question
   step 2::  check base case least optimal solution....
   step 3 :: k-loop (i to j but it will differ question wise )  ..IMP STEP..
-  step 4::  find ans from temp ans by doing max(ans,tmep)(generally)..   */  
+  step 4::  find ans from temp ans by doing max(ans,tmep)(generally)..   */    
 
+
+  // QUESTION :: there is an array for which have to find matrix multiplication maximum... and the order of matrix 
+  //  for A[i]=A[i-1]*A[i].. 
+
+  // input.. arr[n]={10,20,30,40,50};
+  // output will be single integer....
+
+
+     
+#include<bits/stdc++.h>
+using namespace std;   
+
+      int fun(int i,int j, vector<int>&arr){
+             if(i>=j) return 0;
+           int ans=INT_MAX;
+           for(int k=i;k<=j-1;k++){
+               int temp=fun(0,k,arr) +fun(k+1,j,arr) +arr[i-1]*arr[k]*arr[j]; 
+               ans=min(ans,temp);
+           }  
+           return ans; 
+      }
+   
+     int helper(vector<int>&arr){ 
+          int n=arr.size();
+         return fun(1,n-1,arr);
+     }   
+
+int main(){
+     vector<int>arr(5);
+     for(int i=0;i<5;i++) cin>>arr[i];
+      return helper(arr);
+}  
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
   
   
