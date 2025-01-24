eb93b9c131be50a643a7ec6a7187b8adb4bceab9
harshit <samshersinghbaghel152@gmail.com>
2025-01-24 15:24:49 +0530

    new problem

--- LCS/longest palindrome subsequeses.cpp
+++ LCS/longest palindrome subsequeses.cpp
@@ -1,1 +1,23 @@
-//here is the problem
+//we are given two string we have to find the longest common palindromic subsequence...
+
+// input s1="abcdcef", s2="bcdce"
+#include<bits/stdc++.h>
+using namespace  std;
+
+  int fun(int i,int j,string s1,string s2){
+       if(i>=s1.size() || j>=s2.size()) return 0;
+
+       if(s1[i]==s2[j]){
+         return 1+fun(i+1,j+1,s1,s2);
+       }  
+       else{
+         return max(fun(i+1,j,s1,s2),fun(i,j+1,s1,s2));
+       } 
+  }  
+int main(){
+    string s1="abcdcef";
+    string s2="bcdce";
+    reverse(s2.begin(),s2.end());
+
+    cout<<fun(0,0,s1,s2); 
+    }     
