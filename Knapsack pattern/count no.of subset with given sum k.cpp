1b8346253b32a4cfd408fb9fc034dd9caa4faf32
harshit <samshersinghbaghel152@gmail.com>
2025-01-24 14:30:28 +0530

    new problem

--- Knapsack pattern/count no.of subset with given sum k.cpp
+++ Knapsack pattern/count no.of subset with given sum k.cpp
@@ -1,1 +1,20 @@
-//here is the problem
+//we have to count the number the number of subsets such that their sum is equal to K.....
+
+#include<bits/stdc++.h>
+using namespace std;
+ int fun(int i,vector<int>&arr,int k){
+     if(i==arr.size()) return 0; 
+     if(k==0) return 1;
+
+     if(arr[i]<=k){
+        return fun(i+1,arr,k-arr[i])+fun(i+1,arr,k); 
+     }  
+     else{
+         return fun(i+1,arr,k);
+     }
+ } 
+int main(){
+ vector<int>v={1,2,3,4,5};   
+ int k=5;
+ cout<<fun(0,v,k);
+}
