c3b207d25ae490875d0e42252e00c77259c0d825
Samshersingh1 <143586280+Samshersingh1@users.noreply.github.com>
2025-02-19 00:20:53 +0530

    Update equal sum partition.cpp

--- Knapsack pattern/equal sum partition.cpp
+++ Knapsack pattern/equal sum partition.cpp
@@ -1,1 +1,3 @@
 //here is the problem
+// this problem is same as count ot say if there is a subset with sum k is present or not 
+//in this problem the key diffrence is that we have to find k=sum/2.. note k sholud be a integerr
