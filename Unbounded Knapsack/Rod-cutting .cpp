638a6d53a8bea753ca32ae2b2bff603fb68b96d0
Samshersingh1 <143586280+Samshersingh1@users.noreply.github.com>
2025-02-25 20:45:08 +0530

    Update Rod-cutting .cpp
    code added

--- Unbounded Knapsack/Rod-cutting .cpp
+++ Unbounded Knapsack/Rod-cutting .cpp
@@ -35,4 +35,37 @@ int cutRod(vector<int> &price) {
        } 
        return cutRod(arr);  
 
-   } 
+   }  
+
+// Now here ti tabulation of this 
+//tc o(n*n)
+sc:: o(n*n) ..
+   class Solution {
+  public:
+     int maxprofit(int id,int n,vector<int>&arr){
+         if(n==0 || id>=arr.size()) return 0; 
+         
+         int nottake=maxprofit(id+1,n,arr);
+         int take=INT_MIN;
+         if(n>=id+1){
+             take=arr[id]+maxprofit(id,n-(id+1),arr);
+         }
+          return max(take,nottake);
+     }
+    int cutRod(vector<int> &price) {
+        int n=price.size();
+       // return maxprofit(0,n,price); 
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));  
+          for(int i=1;i<=n;i++){
+            for(int k=1;k<=n;k++){
+                 int nottake=dp[i-1][k]; //nottake.... 
+               int take=INT_MIN;
+                 if(k>=i){
+                     take=price[i]+dp[i][k-(i+1)]; //take..
+                }   
+                   dp[i][k]= max(take,nottake);  
+            }
+        } 
+        return dp[n][n]; 
+        
+    }
