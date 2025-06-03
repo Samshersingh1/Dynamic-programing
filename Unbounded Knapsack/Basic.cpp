bf64349c52a285b46a6b1e39a8c0a98271bdc331
Harshit Singh <143586280+Samshersingh1@users.noreply.github.com>
2025-06-03 17:59:01 +0530

    Update Basic.cpp

--- Unbounded Knapsack/Basic.cpp
+++ Unbounded Knapsack/Basic.cpp
@@ -29,4 +29,56 @@ int cutRod(vector<int> &price) {
        } 
        return cutRod(arr);  
 
-   }  
+   }   
+
+// there is  a question Shopping Offers in this question we have to do given 
+ price array ={}, need array={},offers={{},{},{}} we need to find the minimum cost such that all the item i can buy with frequency
+ //    Note :: my item should'nt increase from the given value.... leetcode 638
+ class Solution {
+public:   
+     
+       struct VectorHash {
+        size_t operator()(const vector<int>& v) const {
+            size_t hash = 0;
+            for (int num : v) {
+                hash = hash * 31 + std::hash<int>()(num);
+            }
+            return hash;
+        }
+    };
+
+    
+    unordered_map<vector<int>, int, VectorHash> memo;
+     int knapsack(vector<int>& price, vector<vector<int>>& special, vector<int>& needs){
+         if(memo.count(needs))  return memo[needs];
+         //now we have to count amount..  
+         int minCost=0;
+          int n=price.size();
+          for(int i=0;i<n;i++){
+             minCost+=(price[i]*needs[i]);
+          } 
+
+          for(auto &offer:special){ 
+               bool flag=true;
+              vector<int>newneed(needs);
+              for(int i=0;i<n;i++){
+                if(newneed[i]<offer[i]) {
+                     flag=false; 
+                     break;
+                }  
+                newneed[i]-=offer[i];  
+              }    
+              if(flag) {
+                      int currentCost = offer[n]+knapsack(price,special,newneed);
+                      minCost=min(minCost,currentCost);
+              }   
+
+          }      
+         return   memo[needs] = minCost;   
+     }          
+
+    int shoppingOffers(vector<int>& price, vector<vector<int>>& special, vector<int>& needs) {
+         return knapsack(price,special,needs);
+    }
+};  
+
